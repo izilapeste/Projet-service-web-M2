23ff6798d02eafadecd8198de89274b45335778d
Latifatou <latioue@gmail.com>
2025-06-12 13:50:18 +0000

    miseAjour

---
+++ service-intervention/src/main/java/com/example/intervention/serviceintervention/ServiceInterventionApplication.java
@@ -0,0 +1,11 @@
+package com.example.intervention.serviceintervention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInterventionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceInterventionApplication.class, args);
+    }}
